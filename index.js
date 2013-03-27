d8ec965e4985b652178a7180352d215d37548392
Patrick Kang <jykang1@gmail.com>
2013-03-27 01:05:33 -0400

    adds gitignore file

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 
-			$(document).ready(function(){
-				$(".click").click(function(){
-					$(".open").slideToggle("slow");
-				});
-			});
+$(document).ready(function(){
+	$(".click").click(function(){
+		$(".open").slideToggle("slow");
+	});
+});
 
+$(document).ready(function(){
+	$(".click2").click(function(){
+		$(".open2").slideToggle("slow");
+	});
+});
 
-
-
-			$(document).ready(function(){
-				$(".click2").click(function(){
-					$(".open2").slideToggle("slow");
-				});
-			});
-
-
-
-
-			$(document).ready(function(){
-				$(".click3").click(function(){
-					$(".open3").slideToggle("slow");
-				});
-			});
+$(document).ready(function(){
+	$(".click3").click(function(){
+		$(".open3").slideToggle("slow");
+	});
+});
 
