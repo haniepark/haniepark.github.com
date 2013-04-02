30ace4ac916c2a1cdf283fd8d24636fb56f38fd6
Steph Park <haniepark@gmail.com>
2013-04-01 23:26:19 -0400

    renewal

---
+++ oldhome2/index.js
@@ -0,0 +1,25 @@
+
+			$(document).ready(function(){
+				$(".click").click(function(){
+					$(".open").slideToggle("slow");
+				});
+			});
+
+
+
+
+			$(document).ready(function(){
+				$(".click2").click(function(){
+					$(".open2").slideToggle("slow");
+				});
+			});
+
+
+
+
+			$(document).ready(function(){
+				$(".click3").click(function(){
+					$(".open3").slideToggle("slow");
+				});
+			});
+
